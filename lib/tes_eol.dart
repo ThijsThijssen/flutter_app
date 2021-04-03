a094019a5e20c5ce737e5875762c53975a720df4
ThijsThijssen <thijsthijssen@outlook.com>
2021-04-03 16:27:33 +0200

    initial commit

---
+++ lib/tes_eol.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NewStatefullWidget extends StatefulWidget {
+  @override
+  _NewStatefullWidgetState createState() => _NewStatefullWidgetState();
+}
+
+class _NewStatefullWidgetState extends State<NewStatefullWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
